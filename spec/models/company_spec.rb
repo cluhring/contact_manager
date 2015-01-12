9c3c5ff76a0a861fe17bd39238cf9a663231e76b
Chris Luhring <cluhring@gmail.com>
2015-01-11 22:09:05 -0700

    improved company spec tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -15,8 +15,9 @@ RSpec.describe Company, :type => :model do
     expect(company).to be_invalid
   end
 
-  it "has an array of phone numbers " do
-    expect(company.phone_numbers).to eq([])
+  it "responds with its phone numbers after they're created" do
+    phone_number = company.phone_numbers.build(number: '333-4444')
+    expect(phone_number.number).to eq('333-4444')
   end
 
 end
